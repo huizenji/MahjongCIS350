1d72e8dd727000dde4c780f02546bd7b34ace635
wayne <chenwa@mail.gvsu.edu>
2020-01-16 16:23:53 -0500

    Finished the remaining Tile classes, Did not create the flower class because there was not big difference between that and the tile class.

---
+++ src/MahjongCIS350/Game.java
@@ -0,0 +1,80 @@
+package MahjongCIS350;
+
+import java.util.*;
+
+/**********************************************************************
+ * This class contains all pieces and rule functionality for the game
+ * Mahjong. It is displayed by the Panel and GUI classes.
+ *
+ * @Authors: Jillian Huizenga, Wayne Chen
+ * @Version: 1/15/2020
+ *********************************************************************/
+public class Game {
+
+    private ArrayList<Tile> tiles;
+
+    /******************************************************************
+     * This is the constructor for the Game class.
+     *****************************************************************/
+    public Game(){
+
+        tiles = new ArrayList<>();
+
+        createTile();
+    }
+
+    /*******************************************************************
+     * This method creates every tile in Mahjong.
+     ******************************************************************/
+    private void createTile(){
+
+        createSuiteTile();
+        createPointTile();
+    }
+
+    /*******************************************************************
+     * This method creates all the suites tiles in Mahjong.
+     ******************************************************************/
+    private void createSuiteTile(){
+
+        String []design = {"Circle","Character","Bamboo"};
+
+        for (int index = 0; index < 3; index++) {
+            for (int numtile = 1; numtile <= 9; numtile++) {
+                for (int i = 0; i < 4; i++) {
+
+                    tiles.add(new Suite(design[index], numtile));
+                }
+            }
+        }
+    }
+
+    private void createPointTile(){
+
+        String []dragonColor = {"Red","Green","White"};
+
+        String []windDir = {"North","East","South","West"};
+
+        // creating Dragon Tiles
+        for (int index = 0; index < 3; index++){
+            for (int i = 0; i < 4; i++){
+
+                tiles.add(new Dragon(dragonColor[index]));
+            }
+        }
+
+        // Creating Wind Tiles
+        for (int index = 4; index < 4; index++){
+            for (int i = 0; i < 4; i++){
+
+                tiles.add(new Wind(windDir[index]));
+            }
+        }
+
+        // Creating Flower tiles
+        for (int i = 0; i < 8; i++){
+
+            tiles.add(new Tile("Flower"));
+        }
+    }
+}
