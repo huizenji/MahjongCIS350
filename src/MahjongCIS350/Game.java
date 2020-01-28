31222da6817b6779bfeed1b16dc14dfd9e750b31
AaronZhao <valentinozhao@gmaol.com>
2020-01-27 21:58:39 -0500

    add ischi(), compareconsecutive() of Game class add lastTile() of Player class

--- src/MahjongCIS350/Game.java
+++ src/MahjongCIS350/Game.java
@@ -6,31 +6,41 @@ import java.util.*;
  * This class contains all pieces and rule functionality for the game
  * Mahjong. It is displayed by the Board and GUI classes.
  *
- * @Authors: Jillian Huizenga, Wayne Chen, Aron Zhao
- * @Version: 1/21/2020
+ * @Authors: Jillian Huizenga, Wayne Chen, Xianghe Zhao(Aaron)
+ * @Version: 1/27/2020
  *********************************************************************/
 public class Game {
 
-    /** All the tiles in Mahjong **/
+    /**
+     * All the tiles in Mahjong
+     **/
     private ArrayList<Tile> tiles;
 
     private ArrayList<Tile> discardPile;
 
-    /** The max amount of tiles in Mahjong **/
+    /**
+     * The max amount of tiles in Mahjong
+     **/
     private int maxTile;
 
-    /** Index of current player ,aka, whose turn **/
+    /**
+     * Index of current player ,aka, whose turn
+     **/
     private int currentPlayer;
 
-    /** Index of player who has the "East" direction **/
+    /**
+     * Index of player who has the "East" direction
+     **/
     private int startingPlayer;
 
-    Player []playerList;
+    Player[] playerList;
 
-    /** Total number of players per game **/
+    /**
+     * Total number of players per game
+     **/
     private int TOTAL_PLAYER = 4;
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
         ArrayList<Integer> a = new ArrayList<>();
         ArrayList<Integer> b = new ArrayList<>();
@@ -40,14 +50,14 @@ public class Game {
         b.add(3);
 
         System.out.println(a);
-        a= b;
+        a = b;
         System.out.println(a);
     }
 
     /******************************************************************
      * This is the constructor for the Game class.
      *****************************************************************/
-    public Game(){
+    public Game() {
 
         tiles = new ArrayList<>();
         discardPile = new ArrayList<>();
@@ -61,7 +71,7 @@ public class Game {
     /*******************************************************************
      * This method creates every tile in Mahjong.
      ******************************************************************/
-    private void createTile(){
+    private void createTile() {
 
         createSuiteTile();
         createPointTile();
@@ -70,9 +80,9 @@ public class Game {
     /*******************************************************************
      * This method creates all the suites tiles in Mahjong.
      ******************************************************************/
-    private void createSuiteTile(){
+    private void createSuiteTile() {
 
-        String []design = {"Circle","Bamboo","Character"};
+        String[] design = {"Circle", "Bamboo", "Character"};
 
         for (int index = 0; index < 3; index++) {
             for (int numtile = 1; numtile <= 9; numtile++) {
@@ -88,30 +98,30 @@ public class Game {
     /*******************************************************************
      * This method creates all the point tiles.
      ******************************************************************/
-    private void createPointTile(){
+    private void createPointTile() {
 
-        String []dragonColor = {"Red","Green","White"};
+        String[] dragonColor = {"Red", "Green", "White"};
 
-        String []windDir = {"North","East","South","West"};
+        String[] windDir = {"North", "East", "South", "West"};
 
         // creating Dragon Tiles
-        for (int index = 0; index < 3; index++){
-            for (int i = 0; i < 4; i++){
+        for (int index = 0; index < 3; index++) {
+            for (int i = 0; i < 4; i++) {
 
                 tiles.add(new Dragon(dragonColor[index]));
             }
         }
 
         // Creating Wind Tiles
-        for (int index = 0; index < 4; index++){
-            for (int i = 0; i < 4; i++){
+        for (int index = 0; index < 4; index++) {
+            for (int i = 0; i < 4; i++) {
 
                 tiles.add(new Wind(windDir[index]));
             }
         }
 
         // Creating Flower tiles
-        for (int i = 0; i < 8; i++){
+        for (int i = 0; i < 8; i++) {
 
             tiles.add(new Flower());
         }
@@ -131,7 +141,7 @@ public class Game {
      * This method creates all the players in the game and randomly
      * assigns one of the player to be the starting player.
      ******************************************************************/
-    private void setupPlayer(){
+    private void setupPlayer() {
 
         playerList = new Player[TOTAL_PLAYER];
 
@@ -146,8 +156,8 @@ public class Game {
 
         // Rotate the winds of the player to randomly set a starting
         // player
-        for (int i = 0; i < randVal; i++){
-            for (int j = 0; j < TOTAL_PLAYER; j++){
+        for (int i = 0; i < randVal; i++) {
+            for (int j = 0; j < TOTAL_PLAYER; j++) {
 
                 rotatePlayerDir(playerList[j]);
             }
@@ -161,24 +171,18 @@ public class Game {
     /*******************************************************************
      * This method rotates the wind direction of each player.
      ******************************************************************/
-    private void rotatePlayerDir(Player player){
+    private void rotatePlayerDir(Player player) {
 
-        if (player.getDirection().equals("East")){
+        if (player.getDirection().equals("East")) {
 
             player.setDirection("South");
-        }
-
-        else if (player.getDirection().equals("South")){
+        } else if (player.getDirection().equals("South")) {
 
             player.setDirection("West");
-        }
-
-        else if (player.getDirection().equals("West")){
+        } else if (player.getDirection().equals("West")) {
 
             player.setDirection("North");
-        }
-
-        else if (player.getDirection().equals("North")){
+        } else if (player.getDirection().equals("North")) {
 
             player.setDirection("East");
         }
@@ -188,20 +192,20 @@ public class Game {
      * This method shuffles all the tiles that are not in the players
      * hands.
      ******************************************************************/
-    private void shuffle(){
+    private void shuffle() {
 
         Random rand = new Random();
         int randNum1;
         int randNum2;
 
-        for (int i = 0; i < 100000; i++){
+        for (int i = 0; i < 100000; i++) {
 
             randNum1 = rand.nextInt(maxTile);
             randNum2 = rand.nextInt(maxTile);
 
             Tile temp = tiles.get(randNum2);
-            tiles.set(randNum2,tiles.get(randNum1));
-            tiles.set(randNum1,temp);
+            tiles.set(randNum2, tiles.get(randNum1));
+            tiles.set(randNum1, temp);
         }
     }
 
@@ -209,12 +213,12 @@ public class Game {
      * This method deals out 13 suite tiles to three players, and 14
      * tiles to the East player
      ******************************************************************/
-    private void dealTile_13(){
+    private void dealTile_13() {
 
         // Give out 12 Tiles
         for (int index = 0; index < 4; index++) {
-            for (int i = 0; i < 3; i++){
-                for (int j = 0; j < 4; j++){
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 4; j++) {
                     playerList[index].addTile(tiles.remove(0));
                 }
             }
@@ -222,9 +226,9 @@ public class Game {
 
         // Give out the 13 Tile
         for (int index = 0; index < 4; index++) {
-                for (int j = 0; j < 4; j++){
-                    playerList[index].addTile(tiles.remove(0));
-                }
+            for (int j = 0; j < 4; j++) {
+                playerList[index].addTile(tiles.remove(0));
+            }
         }
         // Giving starting player 1 extra tile
         playerList[startingPlayer].addTile(tiles.remove(0));
@@ -246,12 +250,12 @@ public class Game {
         this.startingPlayer = startingPlayer;
     }
 
-    private void setNextStartingPlayer(){
+    private void setNextStartingPlayer() {
 
         startingPlayer = (startingPlayer + 1) % 4;
     }
 
-    private void setNextCurrentPlayer(){
+    private void setNextCurrentPlayer() {
 
         currentPlayer = (currentPlayer + 1) % 4;
     }
@@ -268,10 +272,36 @@ public class Game {
         this.discardPile = discardPile;
     }
 
-    /** Add to Later Whoever has time **/
-    private void isChi(){
+    /**
+     * To check if there is chi for a specific player
+     *
+     * @param handTile
+     * @param check    the checked_Suite should belongs to the last player of the current player
+     * @return
+     */
+    private boolean isChi(ArrayList<Suite> handTile, Suite check) {
+        //set up lastPlayer of the currentPlayer
+        int lastPlayer = currentPlayer - 1;
 
 
+        int matchdesign = 0;
+        //check if there are 3 same design of suite including the check_Suite
+        for (int i = 0; i < handTile.size(); ++i) {
+            if (handTile.get(i).getDesign().equals(check.getDesign())) {
+                matchdesign++;
+            }
+        }
+        //check if their values are consecutive
+        if (matchdesign >= 2) {
+            for (int i = 0; i < handTile.size() - 1; ++i) {
+                for (int j = i+1; j < handTile.size(); ++j){
+                    if(compareConsecutive(handTile.get(i),handTile.get(j),check)){
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
     }
 
     /*******************************************************************
@@ -281,46 +311,44 @@ public class Game {
      *                 for a pong.
      * @param check The tile checked if it can be made into a pong
      ******************************************************************/
-    private boolean isPong(ArrayList<Suite> handTile, Suite check){
+    private boolean isPong(ArrayList<Suite> handTile, Suite check) {
 
         // Number of Tiles in player hand that can used for a pong
         int matchTile = 0;
 
         // Scan through the hand and determine if there is a matching
         // tile
-        for(int i = 0; i < handTile.size(); i++){
+        for (int i = 0; i < handTile.size(); i++) {
 
-            if (compareSuite(handTile.get(i),check)){
+            if (compareSuite(handTile.get(i), check)) {
 
                 matchTile++;
             }
         }
 
         // If there is 2 or more matching tile, then there is a pong
-        if (matchTile >= 2){
+        if (matchTile >= 2) {
 
             return true;
-        }
-
-        else{
+        } else {
 
             return false;
         }
     }
 
-    private void isKongDiscard(){
+    private void isKongDiscard() {
 
     }
 
-    private void isKongDraw(){
+    private void isKongDraw() {
 
     }
 
-    private void isMahjong(){
+    private void isMahjong() {
 
     }
 
-    private void takePong(Player pl){
+    private void takePong(Player pl) {
 
 
     }
@@ -334,7 +362,7 @@ public class Game {
      * @return
      */
     private ArrayList<Integer> findTile(ArrayList<Tile> playerHand,
-                                        ArrayList<Tile> desired){
+                                        ArrayList<Tile> desired) {
 
         ArrayList index_loc = new ArrayList();
         for (int i = 0; i < desired.size(); i++) {
@@ -343,7 +371,7 @@ public class Game {
                  hand_index++) {
 
 
-                if (compareSuite((Suite)desired.get(i), (Suite)playerHand.get(hand_index))){
+                if (compareSuite((Suite) desired.get(i), (Suite) playerHand.get(hand_index))) {
 
                 }
             }
@@ -359,7 +387,7 @@ public class Game {
      *
      * @param player The player hand that is being sorted.
      ******************************************************************/
-    private void autoSort(Player player){
+    private void autoSort(Player player) {
 
         ArrayList<Tile> temp = new ArrayList<>();
         Suite comp = new Suite(); // Suite that will be placed first
@@ -367,13 +395,13 @@ public class Game {
 
         // Adding Circle Tiles
         comp.setDesign("Circle");
-        for(int i = 0; i < player.getHandTile().size(); i++){
+        for (int i = 0; i < player.getHandTile().size(); i++) {
 
-            playerTile = (Suite)player.getHandTile().get(i);
-            for(int value = 1; value <= 9; value++){
+            playerTile = (Suite) player.getHandTile().get(i);
+            for (int value = 1; value <= 9; value++) {
 
                 comp.setValue(value);
-                if (compareSuite(comp,playerTile));
+                if (compareSuite(comp, playerTile)) ;
 
                 temp.add(playerTile);
             }
@@ -381,13 +409,13 @@ public class Game {
 
         // Adding Bamboo Tiles
         comp.setDesign("Bamboo");
-        for(int i = 0; i < player.getHandTile().size(); i++){
+        for (int i = 0; i < player.getHandTile().size(); i++) {
 
-            playerTile = (Suite)player.getHandTile().get(i);
-            for(int value = 1; value <= 9; value++){
+            playerTile = (Suite) player.getHandTile().get(i);
+            for (int value = 1; value <= 9; value++) {
 
                 comp.setValue(value);
-                if (compareSuite(comp,playerTile));
+                if (compareSuite(comp, playerTile)) ;
 
                 temp.add(playerTile);
             }
@@ -395,13 +423,13 @@ public class Game {
 
         // Adding Character Tiles
         comp.setDesign("Character");
-        for(int i = 0; i < player.getHandTile().size(); i++){
+        for (int i = 0; i < player.getHandTile().size(); i++) {
 
-            playerTile = (Suite)player.getHandTile().get(i);
-            for(int value = 1; value <= 9; value++){
+            playerTile = (Suite) player.getHandTile().get(i);
+            for (int value = 1; value <= 9; value++) {
 
                 comp.setValue(value);
-                if (compareSuite(comp,playerTile));
+                if (compareSuite(comp, playerTile)) ;
 
                 temp.add(playerTile);
             }
@@ -419,26 +447,60 @@ public class Game {
      * @param tile2 The second tile that is being compared.
      * @return true if both tiles are the same.
      ******************************************************************/
-    private boolean compareSuite(Suite tile1, Suite tile2){
+    private boolean compareSuite(Suite tile1, Suite tile2) {
 
-        if (tile1 == null){
+        if (tile1 == null) {
 
             throw new NullPointerException("Argument 1 - tile1 has a" +
                     "null object.\n");
         }
 
-        if (tile2 == null){
+        if (tile2 == null) {
 
             throw new NullPointerException("Argument 2 - tile1 has a" +
                     "null object.\n");
         }
 
         if (tile1.getDesign().equals(tile2.getDesign()) &&
-                tile1.getValue() == tile2.getValue()){
+                tile1.getValue() == tile2.getValue()) {
 
             return true;
         }
 
+        return false;
+    }
+
+    /**
+     * This method compares 3 suite tiles and determine if they are consecutive and belong to the same design of suite
+     * @param tile1
+     * @param tile2
+     * @param tile3
+     * @return
+     */
+    private boolean compareConsecutive(Suite tile1, Suite tile2, Suite tile3){
+        //make an array to hold the three values of the three tiles
+        int[] array = {tile1.getValue(),tile2.getValue(),tile3.getValue()};
+        //find min and max value
+        int min = tile1.getValue();int max = tile1.getValue();
+        for(int i =0; i < 3; i++){
+            if(array[i]<min){
+                min = array[i];
+            }
+        }
+        for(int i = 0; i<3; i++){
+            if(array[i]>max){
+                max = array[i];
+            }
+        }
+        //check if they are consecutive and values are not equal to each other
+        boolean[] visited = new boolean[3];
+        if(tile1.getDesign().equals(tile2.getDesign())&&tile1.getDesign().equals(tile3.getDesign())){
+            if(max-min+1 == 3){
+                if(array[0]!=array[1]&&array[1]!=array[2]&&array[0]!=array[2]){
+                    return true;
+                }
+            }
+        }
         return false;
     }
 }
