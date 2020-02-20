f785f095214c24bae9646b40a17055e9682e407d
AaronZhao <valentinozhao@gmaol.com>
2020-02-20 17:14:28 -0500

    Merge branch 'master' of /Users/zhaoxianghe/Desktop/WInter 2020/CIS350/MahjongCIS350 with conflicts.

--- src/MahjongCIS350/Board.java
+++ src/MahjongCIS350/Board.java
@@ -150,18 +150,7 @@ public class Board extends JPanel {
         // place Panels
         GridBagConstraints c = new GridBagConstraints();
 
-//        JButton b1 = new JButton("Game Reset");//button for the game reset
-//        b1.setBounds(40,0,30,20);
-//        gameBoard.add(b1);
-//        b1.addActionListener(new ActionListener() {
-//            @Override
-//            public void actionPerformed(ActionEvent e) {
-//                if(e.getSource()==b1){
-//                    game.reset();
-//                }
-//                displayBoard();
-//            }
-//        });
+
         c.ipadx = 50;
         c.ipady = 50;
 
@@ -622,7 +611,7 @@ public class Board extends JPanel {
 
         int p1HandSize = game.getPlayerList(0).getHandTile().size();
         int discardPileSize = game.getDiscardPile().size();
-        int drawPileSize = 91 - discardPileSize;
+        int drawPileSize = 144 - discardPileSize;
 
         // update Player1 hand Tiles
         if (p1Hand.size() < p1HandSize){
@@ -643,13 +632,13 @@ public class Board extends JPanel {
         }
 
         // update drawPile when Player or AI draws a Tile (is something still wrong with this?)
-        while (drawPile.size() > drawPileSize){
+        if (drawPile.size() != drawPileSize){
             drawPilePanel.remove(drawPile.get(drawPile.size() - 1));
             drawPile.remove(drawPile.get(drawPile.size() - 1));
         }
 
         // update discardPile when AI discards
-        if (discardPile.size() < discardPileSize) {
+        if (discardPile.size() != discardPileSize) {
             JButton temp = new JButton();
             temp.setPreferredSize(new Dimension(30, 35));
             discardPile.add(temp);
