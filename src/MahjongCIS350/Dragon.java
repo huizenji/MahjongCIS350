535d38eaed3e86b6fb72b5a4326f0a923fe9a56d
Jillian Huizenga <huizenji@mail.gvsu.edu>
2020-02-28 15:15:12 -0500

    Stylized Dragon

--- src/MahjongCIS350/Dragon.java
+++ src/MahjongCIS350/Dragon.java
@@ -1,42 +1,46 @@
 package MahjongCIS350;
 
-/***********************************************************************
- * This class contains the information on the dragon tile in mahjong.
+/**********************************************************************
+ * This class contains the information on the Dragon Tile in Mahjong.
  *
- * @Authors: Jillian Huizenga, Wayne Chen, Aron Zhao
- * @Version: 1/21/2020
- **********************************************************************/
+ * @Authors: Wayne Chen, Jillian Huizenga, Chris Paul, Xianghe Zhao
+ * @Version: 2/28/2020
+ *********************************************************************/
 public class Dragon extends Tile{
 
     /** Color of the Dragon Tile **/
     private String color;
 
-    /*******************************************************************
+    /******************************************************************
      * This ia a constructor that creates for the Dragon class. This
      * only ask for the color of the Dragon.
      *
      * @param color The color of the Dragon tile.
-     ******************************************************************/
+     *****************************************************************/
     public Dragon(String color) {
+
         super("Dragon");
         this.color = color;
     }
 
-    /*******************************************************************
-     * This is the getter method of the dragon tile.
+    /******************************************************************
+     * This is the getter method of the Dragon Tile.
      *
-     * @return Returns the color of the dragon tile.
-     ******************************************************************/
+     * @return the color of the Dragon Tile.
+     *****************************************************************/
     public String getColor() {
+
         return color;
     }
 
-    /*******************************************************************
-     * This method sets the color of the dragon tile.
+    /******************************************************************
+     * This method sets the color of the Dragon Tile.
      *
-     * @param color Color of the dragon tile.
-     ******************************************************************/
+     * @param color color of the Dragon Tile.
+     *****************************************************************/
     public void setColor(String color) {
+
         this.color = color;
     }
+
 }
